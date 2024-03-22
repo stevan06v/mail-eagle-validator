7a8ad17ccc74a6e333d8dc01be65fb12daf81a2c
stevan06v <stevanvlajic5@gmail.com>
2024-03-22 08:01:52 +0100

    feat: design imporved

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
+import tkinter
 import customtkinter
 import os
+import threading
 from tkinter import filedialog as fd
-
-
-def shorten_file_path(file_path, max_length=15, prefix_length=5):
-    if len(file_path) > max_length:
-        prefix = file_path[:prefix_length]
-        suffix = file_path[-(max_length - len(prefix) - 3):]
-        return f"{prefix}...{suffix}"
-    else:
-        return file_path
-
 
 class MyTabView(customtkinter.CTkTabview):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
         self.configure(width=400,
-                       height=400, )
+                       height=400)
 
         # create tabs
         self.add("Single")
@@ -26,22 +18,24 @@ class MyTabView(customtkinter.CTkTabview):
         self.tab("Single").configure(height=200, width=200)
 
         # tab "Single":
+
+        # headline
+        singleLabel = customtkinter.CTkLabel(master=self.tab("Single"), text="Single-Validator", fg_color="transparent",
+                                             font=("System", 20, "bold"))
+        singleLabel.place(relx=.5, rely=.05, anchor=tkinter.CENTER)
+
         def submit():
-            print(emailInput.get())
+            email = emailInput.get()
+            print(email)
 
-        # label for email-field
-        emailLabel = customtkinter.CTkLabel(master=self.tab("Single"), text="Email Address:", fg_color="transparent")
-        emailLabel.grid(row=0, column=0, padx=0, pady=10)
-
-        # email-input-field
-        emailInput = customtkinter.CTkEntry(master=self.tab("Single"), placeholder_text="Email for validation")
-        emailInput.configure(width=200)
-        emailInput.grid(row=0, column=1, padx=0, pady=10)
+        emailInput = customtkinter.CTkEntry(master=self.tab("Single"), width=300, height=40,
+                                            placeholder_text="Email for validation")
+        emailInput.place(relx=.5, rely=.2, anchor=tkinter.CENTER)
 
         # submit-button
-        button = customtkinter.CTkButton(master=self.tab("Single"), text="Validate", command=submit)
-        button.grid(row=1, column=0, padx=20, pady=20, sticky="s")  # Place the button at the bottom
-
+        button = customtkinter.CTkButton(master=self.tab("Single"), text="Validate", width=220, height=40,
+                                         command=submit)
+        button.place(relx=.5, rely=.9, anchor=tkinter.CENTER)  # Place the button at the bottom
 
         # tab "Multiple":
         def submitMultiple():
@@ -49,31 +43,44 @@ class MyTabView(customtkinter.CTkTabview):
 
         def openFileDialog():
             name = fd.askopenfilename()
-            csvFilePathLabel.configure(text=shorten_file_path(os.path.basename(name)))
+            csvFilePathInput.insert(tkinter.END, name)
+
+            print(csvFilePathInput.get())
 
         def optionmenuCallback(choice):
             print("optionmenu dropdown clicked:", choice)
 
-        # filename
-        csvFilePathLabel = customtkinter.CTkLabel(master=self.tab("Multiple"), text="CSV-File:", fg_color="transparent")
-        csvFilePathLabel.grid(row=0, column=0, padx=0, pady=10)
+        # headline
+        multipleLabel = customtkinter.CTkLabel(master=self.tab("Multiple"), text="Multiple-Validator",
+                                               fg_color="transparent",
+                                               font=("System", 20, "bold"))
+        multipleLabel.place(relx=.5, rely=.05, anchor=tkinter.CENTER)
 
-        # open filedialog
-        openFileDialog = customtkinter.CTkButton(master=self.tab("Multiple"), text="Open", command=openFileDialog)
-        openFileDialog.grid(row=0, column=1, padx=0, pady=20)  # Place the button at the bottom
+        # csvFilePath input field
+        csvFilePathInput = customtkinter.CTkEntry(master=self.tab("Multiple"), height=30,
+                                                  placeholder_text="CSV-Filepath")
+        csvFilePathInput.grid(row=0, column=0, padx=(50, 10), pady=(50, 10), sticky="ew")
+
+        # open filedialog button
+        openFileDialog = customtkinter.CTkButton(master=self.tab("Multiple"), text="Open", command=openFileDialog,
+                                                 font=("System", 12))
+        openFileDialog.grid(row=0, column=1, padx=5, pady=(50, 10))
+
+        # option menu label
+        optionMenuLabel = customtkinter.CTkLabel(master=self.tab("Multiple"), text="Delimiter:", fg_color="transparent",
+                                                 font=("System", 12))
+        optionMenuLabel.grid(row=1, column=0, padx=10, pady=5)
 
         # option menu
-        optionMenuLabel = customtkinter.CTkLabel(master=self.tab("Multiple"), text="Delimiter:", fg_color="transparent")
-        optionMenuLabel.grid(row=1, column=0, padx=0, pady=10)
-
         optionmenu = customtkinter.CTkOptionMenu(master=self.tab("Multiple"), values=[",", ";"],
-                                                 command=optionmenuCallback)
+                                                 command=optionmenuCallback, width=2, font=("System", 12))
         optionmenu.set(",")
-        optionmenu.grid(row=1, column=1, padx=20, pady=20)  # Place the button at the bottom
+        optionmenu.grid(row=1, column=1, padx=10, pady=5)
 
-        # submit button for multiple
-        buttonMultiple = customtkinter.CTkButton(master=self.tab("Multiple"), text="Validate", command=submitMultiple)
-        buttonMultiple.grid(row=2, column=0, padx=20, pady=20)  # Place the button at the bottom
+        # submit-button
+        buttonMultiple = customtkinter.CTkButton(master=self.tab("Multiple"), text="Validate", width=220, height=40,
+                                                 command=submitMultiple)
+        buttonMultiple.place(relx=.5, rely=.9, anchor=tkinter.CENTER)  # Place the button at the bottom
 
 
 class App(customtkinter.CTk):
@@ -87,7 +94,7 @@ class App(customtkinter.CTk):
         self.minsize(500, 500)
 
         self.tab_view = MyTabView(master=self)
-        self.tab_view.grid(row=0, column=0, padx=50, pady=30)
+        self.tab_view.place(relx=.5, rely=.5, anchor=tkinter.CENTER)
 
 
 app = App()
